e8ebee8a9bce1050351f4f2c96155ecee319dba7
Raym Avls <utakoloid@outlook.it>
2026-08-05 15:58:16 -0600

    .

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyBdiinWeDPrOFlLEM52M2ijYZr595-leow",
   authDomain: "furros-dijeron.firebaseapp.com",
-  databaseURL: "PEGA_AQUÍ_LA_URL_DE_REALTIME_DATABASE",
+  databaseURL: "https://furros-dijeron-default-rtdb.firebaseio.com",
   projectId: "furros-dijeron",
   storageBucket: "furros-dijeron.firebasestorage.app",
   messagingSenderId: "902025051624",
